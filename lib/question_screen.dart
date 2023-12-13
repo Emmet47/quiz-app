3475c5f85c79dbb0b4f76a62a43ad64bc7ea5976
Emirhan Coşkun <hanemircoskun@gmail.com>
2023-12-13 16:36:27 +0300

    Added QuestionScreen Widget

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:quizz_app/start_screen.dart';
 
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _QuestionScreen();
+  }
+}
+
+class _QuestionScreen extends State {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
