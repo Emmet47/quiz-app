0874cd628894e56c1dedb835a76820d97ac0499f
Emirhan Coşkun <hanemircoskun@gmail.com>
2023-12-22 12:54:21 +0300

    Created ResultScreen Class

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,7 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:quizz_app/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({super.key, required this.choseAnswers});
+
+  final List<String> choseAnswers;
+
+  List<Map<String, Object>> getSummartData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < choseAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i],
+        'correct_answer': questions[i].answers[0],
+        'user_answer': choseAnswers[i],
+      });
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
